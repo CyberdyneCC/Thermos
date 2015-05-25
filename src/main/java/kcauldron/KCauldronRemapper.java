bbf1de17e7b8f90248fac27997aab58afa5b1ce3
Prototik <me@prok.pw>
2015-05-25 20:20:04 +0700

    Fix incompability with obfuscated plugins (like SurvivalGames)

---
+++ src/main/java/kcauldron/KCauldronRemapper.java
@@ -0,0 +1,19 @@
+package kcauldron;
+
+import net.md_5.specialsource.JarMapping;
+import net.md_5.specialsource.JarRemapper;
+
+public class KCauldronRemapper extends JarRemapper {
+	public KCauldronRemapper(JarMapping jarMapping) {
+		super(jarMapping);
+	}
+
+	@Override
+	public String mapSignature(String signature, boolean typeSignature) {
+		try {
+			return super.mapSignature(signature, typeSignature);
+		} catch (Exception e) {
+			return signature;
+		}
+	}
+}
