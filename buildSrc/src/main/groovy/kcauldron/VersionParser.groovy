831c8733cc7870c927bcfe54ff67f82fa7466283
Prototik <me@prok.pw>
2015-06-28 04:35:19 +0700

    Migrate to gitlab ci 7.12

--- buildSrc/src/main/groovy/kcauldron/VersionParser.groovy
+++ buildSrc/src/main/groovy/kcauldron/VersionParser.groovy
@@ -3,16 +3,21 @@ package kcauldron
 import java.util.regex.Matcher
 
 class VersionParser {
-    public static String parseForgeRevision(File forgeFile, File propsFile) {
+    public static String parseForgeVersion(File forgeFile, File propsFile) {
         def forgeVersion = forgeFile.text
         def int majorVersion = v(forgeVersion =~ /.+int majorVersion\s+=\s+(\d+);/)
         def int minorVersion = v(forgeVersion =~ /.+int minorVersion\s+=\s+(\d+);/)
         def int revisionVersion = v(forgeVersion =~ /.+int revisionVersion\s+=\s+(\d+);/)
-        def props = new Properties();
-        propsFile.withInputStream { props.load(it) }
-        def int buildVersion = props['fmlbuild.build.number'] as int
+        def int buildVersion = parseForgeRevision propsFile
         return "${majorVersion}.${minorVersion}.${revisionVersion}.${buildVersion}"
     }
+
+    def static int parseForgeRevision(File propsFile) {
+        def props = new Properties();
+        propsFile.withInputStream { props.load(it) }
+        props['fmlbuild.build.number'] as int
+    }
+
 
     static int v(Matcher matcher) {
         matcher.find()
