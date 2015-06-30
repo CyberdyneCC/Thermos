5dabc4e42ebf6f94db8fd1e340b92709bb6ea2ca
Prototik <me@prok.pw>
2015-07-01 00:53:50 +0700

    Spigot's 0098-Cap-Channel-Registrations.patch (close #82)

--- src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -1059,6 +1059,7 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
     }
 
     public void addChannel(String channel) {
+    	com.google.common.base.Preconditions.checkState( channels.size() < 128, "Too many channels registered" ); // Spigot
         if (channels.add(channel)) {
             server.getPluginManager().callEvent(new PlayerRegisterChannelEvent(this, channel));
         }
