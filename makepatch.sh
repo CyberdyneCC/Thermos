2f37725bce72a0c083af777d588e637f12e21cf1
Robotia <spuri4096@gmail.com>
2016-01-19 01:01:35 -0500

    Make makepatch.sh more useful

--- makepatch.sh
+++ makepatch.sh
@@ -4,12 +4,33 @@ bluf=$(tput setaf 6)
 gref=$(tput setaf 2)
 yelf=$(tput setaf 3)
 reset=$(tput op)
-echo "${bluf}CREATING PATCH${reset}"
-FILE=`basename $@`
-CLEAN=`find eclipse/Clean/src/main/java | grep $FILE`
-CAULD=`find eclipse/cauldron/src/main/java | grep $FILE`
-PATCH=`find patches/ | grep -i $FILE`
-git diff --no-index $CLEAN $CAULD > $PATCH
-echo "Diff of ${redf}$CLEAN${reset} and ${gref}$CAULD${reset} written to ${yelf}$PATCH${reset}"
-
-
+if [ -z $@ ]
+then
+	echo "${redf}Please provide a file to generate.${reset}"
+elif [ $1 == "echo" ]
+then
+	echo "${bluf}CREATING PATCH${reset}"
+	FILE=`basename $2`
+	CLEAN=`find eclipse/Clean/src/main/java | grep $FILE`
+	CAULD=`find eclipse/cauldron/src/main/java | grep $FILE`
+	PATCH=`find patches/ | grep -i $FILE`
+	git diff --no-index $CLEAN $CAULD
+	echo "Diff patch of ${redf}$CLEAN${reset} and ${gref}$CAULD${reset} above."
+elif [ $1 == "make" ]
+then
+	echo "${bluf}CREATING PATCH${reset}"
+	FILE=`basename $2`
+	CLEAN=`find eclipse/Clean/src/main/java | grep $FILE`
+	CAULD=`find eclipse/cauldron/src/main/java | grep $FILE`
+	PATCH=`find patches/ | grep -i $FILE`
+	git diff --no-index $CLEAN $CAULD > $PATCH
+	echo "Diff of ${redf}$CLEAN${reset} and ${gref}$CAULD${reset} written to ${yelf}$PATCH${reset}"
+else
+	echo "${bluf}CREATING PATCH${reset}"
+	FILE=`basename $1`
+	CLEAN=`find eclipse/Clean/src/main/java | grep $FILE`
+	CAULD=`find eclipse/cauldron/src/main/java | grep $FILE`
+	PATCH=`find patches/ | grep -i $FILE`
+	git diff --no-index $CLEAN $CAULD > $PATCH
+	echo "Diff of ${redf}$CLEAN${reset} and ${gref}$CAULD${reset} written to ${yelf}$PATCH${reset}"
+fi
