c01fda0d5072f44d83da0771739ca4dfd82aa250
Robotia <robotia@users.noreply.github.com>
2016-03-07 20:14:06 -0500

    Parallel Travis execution

--- travii/make.sh
+++ travii/make.sh
@@ -5,7 +5,7 @@ cd forge
 git checkout b3a1548cfaac77e9756168a8887270f8181487ad
 cd ..
 git pull origin master
-./gradlew --parallel -PofficialBuild clean setupCauldron jar
+./gradlew --parallel -PofficialBuild clean setupCauldron --parallel jar
 cp build/distributions/Thermos*server* $HOME/release/
 
 # 1492
@@ -13,7 +13,7 @@ cd forge
 git checkout c308a47124394f025bc2ba553646ecf8b6c31ba2
 cd ..
 git pull origin master
-./gradlew --parallel -PofficialBuild clean setupCauldron jar
+./gradlew --parallel -PofficialBuild clean setupCauldron --parallel jar
 cp build/distributions/Thermos*server* $HOME/release
 
 # 1558
@@ -21,7 +21,7 @@ cd forge
 git checkout aa8eaf2b286e809146b7faf4e59ce801a40eab9b
 cd ..
 git pull origin master
-./gradlew --parallel -PofficialBuild clean setupCauldron jar
+./gradlew --parallel -PofficialBuild clean setupCauldron --parallel jar
 cp build/distributions/Thermos*server* $HOME/release
 
 # 1614
@@ -29,7 +29,7 @@ cd forge
 git checkout c6aa04325e8a80af88755adb2339a402e7e90ebb
 cd ..
 git pull origin master
-./gradlew --parallel -PofficialBuild clean setupCauldron jar packageBundle
+./gradlew --parallel -PofficialBuild clean setupCauldron --parallel jar packageBundle
 cp build/distributions/Thermos*server* $HOME/release
 cd build/distributions/
 unzip Thermos*bundle*zip
