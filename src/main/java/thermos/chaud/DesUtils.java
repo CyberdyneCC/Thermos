43f412bcb12185b59d79d1060329e7d0f1d6b676
Robotia <robotia@users.noreply.github.com>
2016-03-23 23:01:16 -0400

    Woops! Minor oversight... (((

--- src/main/java/thermos/chaud/DesUtils.java
+++ src/main/java/thermos/chaud/DesUtils.java
@@ -4,9 +4,8 @@ import net.minecraft.entity.player.EntityPlayer;
 
 public class DesUtils {
 
-    public static boolean isModded()
+    public static boolean isModded(String pkg)
     {
-    	String pkg = sun.reflect.Reflection.getCallerClass().getPackage().getName();
     	return !(pkg.startsWith("net.minecraft") || pkg.startsWith("org.bukkit") || pkg.startsWith("cpw.mods.fml") || pkg.startsWith("org.spigotmc") || pkg.startsWith("thermos"));
     }
 }
