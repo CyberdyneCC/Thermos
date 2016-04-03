096b543a0247b0a4e86b0bad7739bb8f4037f739
Robotia <robotia@users.noreply.github.com>
2016-04-03 13:21:11 -0400

    Chunk hashmap changes

--- src/main/java/thermos/wrapper/ChunkBlockHashMap.java
+++ src/main/java/thermos/wrapper/ChunkBlockHashMap.java
@@ -13,29 +13,31 @@ public class ChunkBlockHashMap {
 	private final org.bukkit.craftbukkit.util.LongObjectHashMap<Chunk[][]> map = new org.bukkit.craftbukkit.util.LongObjectHashMap<Chunk[][]>();
 	private int size = 0;
 
-	public static long chunk_hash(int x, int z)
+	/*public static long chunk_hash(int x, int z)
 	{
 		//return ((x & 0xFFFF) << 16) | (z & 0xFFFF);
 //		long key = LongHash.toLong(x, z);
 //		return LongHash.toLong((int) (key & 0xFFFFFFFFL), (int) (key >>> 32));
 		return (((long)x)<<32L)^z;
-	}
+	}*/
 
-	private static int chunk_array(int index)
+	/*private static int chunk_array(int index)
 	{
-		return Math.abs(index % 16);
+		index = index % 16;
+		return (index + (index >> 31)) ^ (index >> 31);
 	}
-
-	private Chunk[][] chunk_array_get(int x, int z)
+	*/
+	/*private Chunk[][] chunk_array_get(int x, int z)
 	{
 		Chunk[][] bunch = this.map.get(chunk_hash(x >> 4, z >> 4));
 		return bunch;
-	}
-	private Chunk[][] chunk_array_remove(int x, int z)
+	}*/
+	
+	/*private Chunk[][] chunk_array_remove(int x, int z)
 	{
 		Chunk[][] bunch = this.map.remove(chunk_hash(x >> 4, z >> 4));
 		return bunch;
-	}
+	}*/
 
 	public org.bukkit.craftbukkit.util.LongObjectHashMap<Chunk[][]> raw()
 	{
@@ -68,9 +70,13 @@ public class ChunkBlockHashMap {
 			return last4;
 		}   
 		
-		Chunk[][] bunch = this.map.get(chunk_hash(x >> 4, z >> 4));
+		Chunk[][] bunch = this.map.get((((long)(x>>4))<<32L)^(z>>4));
 		if(bunch == null) return null;
-		Chunk ref = bunch[Math.abs(x % 16)][Math.abs(z % 16)];
+		
+		x %= 16;
+		z %= 16;
+		Chunk ref = bunch[(x + (x >> 31)) ^ (x >> 31)][(z + (z >> 31)) ^ (z >> 31)];
+		
 		if ( ref != null)
 		{
 			last4 = last3; 
@@ -87,39 +93,56 @@ public class ChunkBlockHashMap {
 			return;
 		size++;
 		int x = chunk.xPosition, z = chunk.zPosition;
-		Chunk[][] temp_chunk_bunch = chunk_array_get(x, z);
+		
+		long chunkhash = (((long)(x>>4))<<32L)^(z>>4);
+		
+		Chunk[][] temp_chunk_bunch = this.map.get(chunkhash);
+		
+		x %= 16;
+		z %= 16;
+		
+		x = (x + (x >> 31)) ^ (x >> 31);
+		z = (z + (z >> 31)) ^ (z >> 31);
+		
 		if(temp_chunk_bunch != null)
 		{
-			temp_chunk_bunch[chunk_array(x)][chunk_array(z)] = chunk;
+			temp_chunk_bunch[x][z] = chunk;
 		}
 		else
 		{
 			temp_chunk_bunch = new Chunk[16][16];
-			temp_chunk_bunch[chunk_array(x)][chunk_array(z)] = chunk;
-			this.map.put(chunk_hash(x >> 4, z >> 4), temp_chunk_bunch); //Thermos - IntHash
+			temp_chunk_bunch[x][z] = chunk;
+			this.map.put(chunkhash, temp_chunk_bunch); //Thermos
 		}
-		/*if ( chunk != null)
+		if ( chunk != null)
 		{
 			last4 = last3; 
 			last3 = last2; 
 			last2 = last1;
 			last1 = chunk;
-		}*/
+		}
 	}
 
 	public void remove(Chunk chunk)
 	{
 		int x = chunk.xPosition, z = chunk.zPosition;
-		Chunk[][] temp_chunk_bunch = chunk_array_get(x, z);
+		Chunk[][] temp_chunk_bunch = this.map.get((((long)(x>>4))<<32L)^(z>>4));
+		
+		x %=16;
+		z %=16;
+		
+		x = (x + (x >> 31)) ^ (x >> 31);
+		z = (z + (z >> 31)) ^ (z >> 31);		
+		
 		if(temp_chunk_bunch != null)
 		{
-			if(temp_chunk_bunch[chunk_array(x)][chunk_array(z)] != null)
+			if(temp_chunk_bunch[x][z] != null)
 			{
 				size--;
-				temp_chunk_bunch[chunk_array(x)][chunk_array(z)] = null;
+				temp_chunk_bunch[x][z] = null;
 			}
 		}
-		if(last1 != null && last1.xPosition == x && last1.zPosition == z)
+		if(last1 != null && last1.xPosition == chunk.xPosition && last1.zPosition == chunk.zPosition)
 		{
 			last1 = null;
 			last1 = last2;
@@ -127,20 +150,20 @@ public class ChunkBlockHashMap {
 			last3 = last4;
 			last4 = null;
 		}
-		if(last2 != null && last2.xPosition == x && last2.zPosition == z)
+		if(last2 != null && last2.xPosition == chunk.xPosition && last2.zPosition == chunk.zPosition)
 		{
 			last2 = null;
 			last2 = last3;
 			last3 = last4;
 			last4 = null;
 		}
-		if(last3 != null && last3.xPosition == x && last3.zPosition == z)
+		if(last3 != null && last3.xPosition == chunk.xPosition && last3.zPosition == chunk.zPosition)
 		{
 			last3 = null;
 			last3 = last4; 
 			last4 = null;
 		}
-		if(last4 != null && last4.xPosition == x && last4.zPosition == z)
+		if(last4 != null && last4.xPosition == chunk.xPosition && last4.zPosition == chunk.zPosition)
 		{
 			last4 = null;
 		}       
